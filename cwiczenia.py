068b745fe34f39cafe0bc60764b99f5d29074de2
jozieblo <73527284+jozieblo@users.noreply.github.com>
2021-11-25 00:39:20 +0100

    Add files via upload

---
+++ cwiczenia.py
@@ -0,0 +1,45 @@
+# zad1
+# def numbers(n):
+#     if n < 0:
+#         return 0
+#     else:
+#         print(n)
+#         numbers(n-1)
+# numbers(5)
+
+# zad2
+# def fib(n):
+#     if n < 1:
+#         return 0
+#     if n < 2:
+#         return 1
+#     else:
+#         return fib(n-1)+fib(n-2)
+#
+# print(fib(18))
+
+# zad3
+
+# def power(x,y):
+#     if y == 0:
+#         return 1
+#     else:
+#         return x* power(x,y-1)
+# print(power(2,4))
+
+# zad4
+
+# def reverse(tekst):
+#     if len(tekst)==0:
+#         return " "
+#     return tekst[-1] +reverse(tekst)
+#
+# print(reverse("kora"))
+
+#zad5
+# def factoral(n):
+#     if n<=1:
+#         return 1
+#     else:
+#         return n * factoral(n-1)
+# print(factoral(0))
